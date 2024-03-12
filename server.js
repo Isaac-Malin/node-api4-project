9892a525ee1a14ee98974e4b9137944fcde7ff1f
Isaac Malin <Isaacmalin3530@gmail.com>
2024-03-11 20:32:17 -0600

    api is working and get request to get users to also working

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const server = express()
 
+const users = [
+  {
+    "name": "Isaac",
+    "age": 22,
+    "email": "Isaacmalin@gmail.com"
+  },
+  {
+    "name": "Brooke",
+    "age": 24,
+    "email": "Brookemalin@gmail.com"
+  },
+  {
+    "name": "Hudson",
+    "age": 2,
+    "email": "Hudsonmalin@gmail.com"
+  },
+  {
+    "name": "Blakelee",
+    "age": 1,
+    "email": "Blakeleemalin@gmail.com"
+  }
+]
+
+server.get("/api/users", (req, res) => {
+  res.json(users)
+})
+
 server.use(express.json())
 
 module.exports = server
